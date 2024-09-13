e4e55e0b39ba0eb089dbc674ba09ac71a374b2e7
ItsSosha <whoissosha@gmail.com>
2024-09-13 10:15:31 +0200

    fix: search layout on mobile

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { AppShell, Box, Container, Group, Text } from "@mantine/core";
+import { AppShell, Container, Flex, Grid, Stack, Text } from "@mantine/core";
 import { PropsWithChildren } from "react";
 import { GlobalError, LocalePicker, ScrollToTop } from "../widgets";
 import { ErrorBoundary } from "react-error-boundary";
@@ -6,25 +6,30 @@ import { ErrorBoundary } from "react-error-boundary";
 export const MainLayout = ({ children }: PropsWithChildren) => (
   <AppShell
     header={{
-      height: 80,
+      height: { base: 140, xs: 70, sm: 60 },
     }}
   >
     <AppShell.Header>
-      <Group align="center" h="100%" component={Container}>
-        <Box flex="1 0 0" />
-        <Text
-          flex="3 0 0"
-          ta="center"
-          fw={800}
-          fz={"h2"}
-          style={{ fontStretch: "125%" }}
-        >
-          GraphQL Github User Search App
-        </Text>
-        <Box flex="1 0 0">
-          <LocalePicker />
-        </Box>
-      </Group>
+      <Flex component={Container} align="center" size="lg" h={"100%"}>
+        <Grid w="100%">
+          <Grid.Col span={{ base: 0, xs: 2 }} />
+          <Grid.Col span={{ base: 12, xs: 8 }} h="100%">
+            <Text
+              ta="center"
+              fw={800}
+              fz={{ base: "h4", xs: "h3", sm: "h2" }}
+              style={{ fontStretch: "125%" }}
+            >
+              GraphQL Github User Search App
+            </Text>
+          </Grid.Col>
+          <Grid.Col span={{ base: 12, xs: 2 }}>
+            <Stack h="100%" justify="center">
+              <LocalePicker />
+            </Stack>
+          </Grid.Col>
+        </Grid>
+      </Flex>
     </AppShell.Header>
     <AppShell.Main>
       <ErrorBoundary FallbackComponent={GlobalError}>
